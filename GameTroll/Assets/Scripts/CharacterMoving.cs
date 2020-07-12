b48aa7fcdb65037c5c524c7042299da7b447cde9
LeeHoanMinh <LeeHoanMinh@gmail.com>
2020-07-12 19:14:51 +0700

    Final Test

---
+++ GameTroll/Assets/Scripts/CharacterMoving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoving : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    void Start()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
---
+++ GameTroll/Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator ani;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ani.SetBool("Idle", true);
+            ani.SetBool("Run", false);
+            ani.SetBool("Jump", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ani.SetBool("Idle", false);
+            ani.SetBool("Run", true);
+            ani.SetBool("Jump", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ani.SetBool("Idle", false);
+            ani.SetBool("Run", false);
+            ani.SetBool("Jump", true);
+        }
+    }
+}
